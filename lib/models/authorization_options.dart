14003804832cfd50ecb4033e72f61e24f1a3c526
notapineapple <damnyorch@gmail.com>
2020-11-26 21:38:31 -0600

    chore: we have some stuff to do :(

--- lib/models/authorization_options.dart
+++ lib/models/authorization_options.dart
@@ -3,7 +3,13 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'authorization_options.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class AuthorizationOptions {
+  // TODO: implement AuthorizationOptions for iOS
 
+  AuthorizationOptions();
+
+  factory AuthorizationOptions.fromJson(Map<String, dynamic> json) => _$AuthorizationOptionsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthorizationOptionsToJson(this);
 }
